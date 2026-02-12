bce9fe79d4f595b4dcb95c48b79fe68693ead31c
Tyler Harold <tyler.harold@nitrado.net>
2026-02-12 16:35:38 -0500

    feat(controllers): add installer/install_controller.ts

---
+++ app/controllers/installer/install_controller.ts
@@ -0,0 +1,10 @@
+// import type { HttpContext } from '@adonisjs/core/http'
+
+export default class InstallController {
+  handle() {
+    // TODO: Single-action controller for setting up a Roost instance.
+    // - Handles basic server settings
+    // - Creates the owner user
+    // - Sets the "APP_INSTALLED" & "APP_MAINTENANCE" global flag to false.
+  }
+}
