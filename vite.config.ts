aaba9e96403a1c56d9e11d66c9898abf35686689
Tyler Harold <tyler.harold@nitrado.net>
2026-02-12 13:58:05 -0500

    feat(app): tailwind setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,15 @@ import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
 import react from '@vitejs/plugin-react'
 import adonisjs from '@adonisjs/vite/client'
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  plugins: [inertia({ ssr: { enabled: false } }), react(), adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] })],
+  plugins: [
+    inertia({ ssr: { enabled: false } }),
+    react(),
+    tailwindcss(),
+    adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] }),
+  ],
 
   /**
    * Define aliases for importing modules from
