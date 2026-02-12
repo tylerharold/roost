205faaa519f1bdb3028147775d8bcad9e859ff90
Tyler Harold <tyler.harold@nitrado.net>
2026-02-12 16:02:57 -0500

    refactor(pages): add placeholder 'not available' content to home page

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -3,7 +3,12 @@ import { Head } from '@inertiajs/react'
 export default function Home() {
   return (
     <>
-      <Head title="App" />
+      <Head title="Not Available" />
+
+      <div className="flex flex-col justify-center mx-auto items-center text-center w-full h-screen">
+        <h1 className="text-2xl text-neutral-50 font-bold">Roost is currently unavailable.</h1>
+        <p className="text-md text-neutral-100">Please contact your server administrator for more information.</p>
+      </div>
     </>
   )
 }
