374714e49054b374ddd90d16f3fe3fe381c0e966
Tyler Harold <tyler@programmer.net>
2026-02-14 15:00:41 -0500

    feat(app): add getFlag/getSetting & maintenance/installed util funcs

--- app/services/server_service.ts
+++ app/services/server_service.ts
@@ -2,6 +2,22 @@ import ServerSetting from '#models/server_setting'
 import ServerFlag from '#models/server_flag'
 
 export default class ServerService {
+  public async getSetting(key: string, options?: { client?: any }): Promise<string | null> {
+    const query = options?.client
+      ? ServerSetting.query({ client: options.client })
+      : ServerSetting.query()
+    const setting = await query.where('key', key).first()
+    return setting?.value ?? null
+  }
+
+  public async getFlag(key: string, options?: { client?: any }): Promise<boolean | null> {
+    const query = options?.client
+      ? ServerFlag.query({ client: options.client })
+      : ServerFlag.query()
+    const flag = await query.where('key', key).first()
+    return flag?.enabled ?? null
+  }
+
   public async updateSetting(key: string, value: string, options?: { client?: any }) {
     const updateData = { value }
 
@@ -20,5 +36,15 @@ export default class ServerService {
     } else {
       await ServerFlag.updateOrCreate({ key }, updateData)
     }
+  }
+
+  public async isInstalled(options?: { client?: any }): Promise<boolean> {
+    const installed = await this.getFlag('APP_INSTALLED', options)
+    return installed ?? false
+  }
+
+  public async isInMaintenance(options?: { client?: any }): Promise<boolean> {
+    const maintenance = await this.getFlag('APP_MAINTENANCE', options)
+    return maintenance ?? false
   }
 }
