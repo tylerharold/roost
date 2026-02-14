2f2ceedcad55782e69ace53ab98787ae92a3a775
Tyler Harold <tyler@programmer.net>
2026-02-14 15:28:25 -0500

    refactor(pages): update install form to use useForm inertia util

--- inertia/pages/install.tsx
+++ inertia/pages/install.tsx
@@ -1,7 +1,24 @@
-import { Form, Head } from '@inertiajs/react'
+import { Head, useForm } from '@inertiajs/react'
 import { Input, Label, Panel, PrimaryButton, TextArea } from '~/components/ui'
+import type { FormEvent } from 'react'
 
 export default function Install() {
+  const { data, setData, post, processing, errors, clearErrors } = useForm({
+    server: {
+      name: '',
+      description: '',
+    },
+    owner: {
+      username: '',
+      password: '',
+    },
+  })
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    post('/api/v1/installer/install')
+  }
+
   return (
     <>
       <Head title="Install" />
@@ -12,26 +29,44 @@ export default function Install() {
           <p className="text-neutral-300">Welcome to Roost!</p>
         </div>
         <Panel className="lg:min-w-2xl max-w-3xl">
-          <Form action="/api/v1/installer/install" className="space-y-8">
+          <form onSubmit={handleSubmit} className="space-y-8">
             <div className="flex flex-col space-y-2">
               <span className="text-neutral-100 text-lg font-medium">Server Settings</span>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
                 <Label htmlFor="server_name">Server Name</Label>
-                <Input
-                  type="text"
-                  id="server_name"
-                  name="server_name"
-                  placeholder="My awesome Roost server!"
-                />
+                <div className="flex flex-col space-y-1">
+                  <Input
+                    type="text"
+                    id="server_name"
+                    value={data.server.name}
+                    onChange={(e) => {
+                      clearErrors('server.name')
+                      setData('server.name', e.target.value)
+                    }}
+                    placeholder="My awesome Roost server!"
+                  />
+                  {errors['server.name'] && (
+                    <span className="text-red-400 text-sm">{errors['server.name']}</span>
+                  )}
+                </div>
               </div>
 
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
                 <Label htmlFor="server_description">Server Description</Label>
-                <TextArea
-                  id="server_description"
-                  name="server_description"
-                  placeholder="My awesome Roost server!"
-                />
+                <div className="flex flex-col space-y-1">
+                  <TextArea
+                    id="server_description"
+                    value={data.server.description}
+                    onChange={(e) => {
+                      clearErrors('server.description')
+                      setData('server.description', e.target.value)
+                    }}
+                    placeholder="My awesome Roost server!"
+                  />
+                  {errors['server.description'] && (
+                    <span className="text-red-400 text-sm">{errors['server.description']}</span>
+                  )}
+                </div>
               </div>
             </div>
 
@@ -39,33 +74,47 @@ export default function Install() {
               <span className="text-neutral-100 text-lg font-medium">Owner User</span>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
                 <Label htmlFor="owner_username">Username</Label>
-                <Input
-                  type="text"
-                  id="owner_username"
-                  name="owner_password"
-                  placeholder="admin"
-                />
+                <div className="flex flex-col space-y-1">
+                  <Input
+                    type="text"
+                    id="owner_username"
+                    value={data.owner.username}
+                    onChange={(e) => {
+                      clearErrors('owner.username')
+                      setData('owner.username', e.target.value)
+                    }}
+                    placeholder="admin"
+                  />
+                  {errors['owner.username'] && (
+                    <span className="text-red-400 text-sm">{errors['owner.username']}</span>
+                  )}
+                </div>
               </div>
 
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
-                <Label htmlFor="server_description">Password</Label>
-                <Input
-                  type="password"
-                  id="owner_password"
-                  name="owner_password"
-                  placeholder="yH`1u0£s8\Kv"
-                />
+                <Label htmlFor="owner_password">Password</Label>
+                <div className="flex flex-col space-y-1">
+                  <Input
+                    type="password"
+                    id="owner_password"
+                    value={data.owner.password}
+                    onChange={(e) => {
+                      clearErrors('owner.password')
+                      setData('owner.password', e.target.value)
+                    }}
+                    placeholder="yH`1u0£s8\Kv"
+                  />
+                  {errors['owner.password'] && (
+                    <span className="text-red-400 text-sm">{errors['owner.password']}</span>
+                  )}
+                </div>
               </div>
             </div>
 
-            <PrimaryButton
-              type="submit"
-              className="w-full"
-            >
-              Create Server
+            <PrimaryButton type="submit" className="w-full" disabled={processing}>
+              {processing ? 'Creating Server...' : 'Create Server'}
             </PrimaryButton>
-
-          </Form>
+          </form>
         </Panel>
       </div>
     </>
