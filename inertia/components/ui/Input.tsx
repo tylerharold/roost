26560d2f59c11f8b00600c0d31bdb1b956047510
Tyler Harold <tyler@programmer.net>
2026-02-14 08:14:24 -0500

    feat(components): basic Input

---
+++ inertia/components/ui/Input.tsx
@@ -0,0 +1,17 @@
+import { forwardRef } from 'react'
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(({ className = '', ...props }, ref) => {
+  return (
+    <input
+      ref={ref}
+      className={`px-4 py-2 bg-neutral-700 border border-neutral-600 text-neutral-100 rounded-lg focus:outline-none focus:ring-2 focus:ring-lime-500 focus:border-transparent transition-colors ${className}`}
+      {...props}
+    />
+  )
+})
+
+Input.displayName = 'Input'
+
+export default Input
