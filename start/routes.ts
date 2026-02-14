c1f35971bdcf2aa9d7398b232e020f10f9eb1b21
Tyler Harold <tyler@programmer.net>
2026-02-14 09:18:45 -0500

    feat(routes): add /api/v1/ group route scaffold + install routes

--- start/routes.ts
+++ start/routes.ts
@@ -8,5 +8,21 @@
 */
 
 import router from '@adonisjs/core/services/router'
+
+import InstallController from '#controllers/installer/install_controller'
+
 router.on('/').renderInertia('home')
+router.on('/install').renderInertia('install')
+
+// API
+router.group(() => {
+  // v1
+  router.group(() => {
+    router.group(() => {
+      router.post('install', [InstallController, 'install'])
+    }).prefix('/installer')
+  }).prefix('/v1')
+}).prefix('/api')
+
+
 
