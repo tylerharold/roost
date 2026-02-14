8560242d27b14b7065572b26b0897eb46c46c5dc
Tyler Harold <tyler@programmer.net>
2026-02-14 15:26:01 -0500

    feat(app): add home controller

---
+++ app/controllers/home_controller.ts
@@ -0,0 +1,28 @@
+import ServerService from '#services/server_service';
+import { inject } from '@adonisjs/core';
+import type { HttpContext } from '@adonisjs/core/http'
+
+@inject()
+export default class HomeController {
+  constructor(
+    protected serverService: ServerService
+  ) {}
+
+  async handle({ inertia, response }: HttpContext) {
+    let payload: any = {}
+
+    const installed = await this.serverService.isInstalled()
+    payload['installed'] = installed
+
+    if (installed) {
+      const serverName = await this.serverService.getSetting('SERVER_NAME')
+      const serverDescription = await this.serverService.getSetting('SERVER_DESCRIPTION')
+
+      payload['server'] = {}
+      payload['server']['name'] = serverName
+      payload['server']['description'] = serverDescription
+    }
+
+    return inertia.render('home', payload)
+  }
+}
