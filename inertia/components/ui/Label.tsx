02ff40bb053f1c58c00d28d772b1bf299bc5c8c6
Tyler Harold <tyler@programmer.net>
2026-02-14 08:49:13 -0500

    refactor(css): adjust label primitive to follow radix example

--- inertia/components/ui/Label.tsx
+++ inertia/components/ui/Label.tsx
@@ -1,17 +1,21 @@
 import { forwardRef } from 'react'
-import { Label as LabelPrimitive } from '@radix-ui/react-label'
+import * as LabelPrimitive from '@radix-ui/react-label'
 
-export interface LabelProps extends React.ComponentProps<typeof LabelPrimitive> {}
+export interface LabelProps extends React.ComponentProps<typeof LabelPrimitive.Root> {}
 
-const Label = forwardRef<HTMLLabelElement, LabelProps>(({ className = '', ...props }, ref) => {
-  return (
-    <LabelPrimitive
-      ref={ref}
-      className={`text-sm text-neutral-400 font-medium ${className}`}
-      {...props}
-    />
-  )
-})
+const Label = forwardRef<HTMLLabelElement, LabelProps>(
+  ({ className = '', children, ...props }, ref) => {
+    return (
+      <LabelPrimitive.Root
+        ref={ref}
+        className={`text-sm text-neutral-400 font-medium ${className}`}
+        {...props}
+      >
+        {children}
+      </LabelPrimitive.Root>
+    )
+  }
+)
 
 Label.displayName = 'Label'
 
