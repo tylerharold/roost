9dfb8059708dbc29a0d8c61a57045358f80c3061
Tyler Harold <tyler@programmer.net>
2026-02-15 10:57:20 -0500

    fix(app): whoops, add server icon to home controller

--- app/controllers/home_controller.ts
+++ app/controllers/home_controller.ts
@@ -1,14 +1,12 @@
-import ServerService from '#services/server_service';
-import { inject } from '@adonisjs/core';
+import ServerService from '#services/server_service'
+import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
 
 @inject()
 export default class HomeController {
-  constructor(
-    protected serverService: ServerService
-  ) {}
+  constructor(protected serverService: ServerService) {}
 
-  async handle({ inertia, response }: HttpContext) {
+  async handle({ inertia }: HttpContext) {
     let payload: any = {}
 
     const installed = await this.serverService.isInstalled()
@@ -17,10 +15,12 @@ export default class HomeController {
     if (installed) {
       const serverName = await this.serverService.getSetting('SERVER_NAME')
       const serverDescription = await this.serverService.getSetting('SERVER_DESCRIPTION')
+      const serverIcon = await this.serverService.getServerIconPath()
 
       payload['server'] = {}
       payload['server']['name'] = serverName
       payload['server']['description'] = serverDescription
+      payload['server']['icon'] = serverIcon
     }
 
     return inertia.render('home', payload)
