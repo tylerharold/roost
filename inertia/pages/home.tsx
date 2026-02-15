14f21955909354af914ba40ec260082b272e8b94
Tyler Harold <tyler@programmer.net>
2026-02-15 10:56:45 -0500

    feat(pages): display server icon on home

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -5,6 +5,7 @@ type HomeProps = {
   installed: boolean
   server?: {
     name: string
+    icon?: string
     description?: string
   }
 }
@@ -17,6 +18,7 @@ export default function Home(props: HomeProps) {
       {props.installed ? (
         <div className="flex flex-col justify-center mx-auto items-center text-center w-full h-screen sm:px-0 px-6">
           <Panel className="sm:w-xl w-full">
+            <img src={props.server!.icon} className="rounded-full" />
             <h1 className="text-2xl text-neutral-50 font-bold">{props.server!.name}</h1>
             <p className="text-md text-neutral-100">{props.server!.description}</p>
           </Panel>
@@ -25,7 +27,9 @@ export default function Home(props: HomeProps) {
         <div className="flex flex-col justify-center mx-auto items-center text-center w-full h-screen">
           <Panel>
             <h1 className="text-2xl text-neutral-50 font-bold">Roost is currently unavailable.</h1>
-            <p className="text-md text-neutral-100">Please contact your server administrator for more information.</p>
+            <p className="text-md text-neutral-100">
+              Please contact your server administrator for more information.
+            </p>
           </Panel>
         </div>
       )}
